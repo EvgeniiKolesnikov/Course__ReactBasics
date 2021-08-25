985b3f533822d69f42d073e9e94a435ee9293c06
Evgenii Kolesnikov <jekow@yandex.ru>
2021-08-25 19:45:25 +0300

    Add useReducer

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {useAlert} from './Alert/AlertContext'
 export default function Main() {
-  const {toggle} = useAlert();
+  const {show} = useAlert();
   
   return (
     <>
       <h1>Sample</h1>
-      <button onClick={toggle}>Show alert</button>
+      <button onClick={()=> show('Text from Main.js')}>Show alert</button>
     </>
   )
 }
